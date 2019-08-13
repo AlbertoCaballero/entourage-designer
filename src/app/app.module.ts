43a70f8ab011cf06628c207d321e9142587393db
Alberto Caballero <albertoCaballero80@outlook.com>
2019-08-13 15:05:02 -0500

    Initial demo setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ViewboxComponent } from './viewbox/viewbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewboxComponent
   ],
   imports: [
     BrowserModule
