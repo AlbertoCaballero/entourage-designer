05244dd1bba1408ee60fec010cd37f0706d124a8
Alberto Caballero <albertoCaballero80@outlook.com>
2019-08-13 22:57:43 -0500

    Added basic components and properties for future functionality

--- src/app/viewbox/viewbox.component.ts
+++ src/app/viewbox/viewbox.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+const imputs = document.querySelectorAll('.sliders input');
+const image = document.getElementById('image');
+
 @Component({
   selector: 'app-viewbox',
   templateUrl: './viewbox.component.html',
   styleUrls: ['./viewbox.component.scss']
 })
+
 export class ViewboxComponent implements OnInit {
+
+  rangex : number = 50;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
