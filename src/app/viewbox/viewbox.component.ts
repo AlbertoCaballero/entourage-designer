578c6178f18fa3150d16dd308927d296ce2751bb
Alberto Caballero <albertoCaballero80@outlook.com>
2019-08-14 22:43:01 -0500

    Decripted the math function to keep the shadow in position

--- src/app/viewbox/viewbox.component.ts
+++ src/app/viewbox/viewbox.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-const imputs = document.querySelectorAll('.sliders input');
-const image = document.getElementById('image');
-
 @Component({
   selector: 'app-viewbox',
   templateUrl: './viewbox.component.html',
@@ -11,11 +8,20 @@ const image = document.getElementById('image');
 
 export class ViewboxComponent implements OnInit {
 
+  //Retriveing the image references
+  imputs : any = document.querySelectorAll('.sliders input');
+  image : any = document.getElementById('image');
+
+  //The parameters that will be updated
   rangex : number = 50;
+  rangey : number = 50;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngDoCheck() {
+    //Every time something changes this gets executed
 
   }
 
