6cd57be9709e586c33509837ba1ac90e6dd9584a
Alberto Caballero <albertoCaballero80@outlook.com>
2019-08-20 21:45:24 -0500

    Removed unecesary lines

--- src/app/viewbox/viewbox.component.ts
+++ src/app/viewbox/viewbox.component.ts
@@ -14,7 +14,7 @@ export class ViewboxComponent implements OnInit {
 
   //The parameters that will be updated
   rangex : number = 0;
-  rangey : number = 0.5;
+  rangey : number = 1;
 
   //Properties to change
   translatey : number = 0;
@@ -33,10 +33,6 @@ export class ViewboxComponent implements OnInit {
 
   //Every time something changes this gets executed. First I need to change the values using the input.
   ngDoCheck() {
-    //Reverse the previous changes
-    document.getElementById("shadow").style.transform = 
-      `translateY(${-this.translatey}px) skewX(${-this.skewx}deg) translateX(${this.translatex}px) scaleY(${-this.scaley})`;
-
     //Recalculates the values
     this.skewx = this.rangex;
     this.scaley = this.rangey;
@@ -46,13 +42,18 @@ export class ViewboxComponent implements OnInit {
 
     this.translatey = (250*(1-this.scaley))/2;
 
-    //Applies the new values
+    //Applies the new values for the shadow
     document.getElementById("shadow").style.transform = 
       `translateY(${this.translatey}px) skewX(${this.skewx}deg) translateX(${-this.translatex}px) scaleY(${this.scaley})`;
 
     //Modify gradient overlay properties
     document.getElementById("gradient-overlay").style.background = 
-      `linear-gradient(90deg, rgba(0,0,0,0) 0%, rgb(212, 37, 37) ${ this.calculateLighting(this.skewx) }%, rgb(255, 255, 255) 50%, rgba(0,0,0,0) 100%)`;
+      `linear-gradient(90deg, rgba(0,0,0,0) 0%, rgb(255, 255, 255, 1) ${ this.calculateLighting(this.skewx) }%, rgba(0,0,0,0) 100%)`;
+      //`linear-gradient(90deg, rgba(0,0,0,0) 0%, rgb(0, 0, 0, 0.5) ${ 100 - this.calculateLighting(this.skewx) }%, 
+      //rgb(255, 0, 255, 0.5) ${ this.calculateLighting(this.skewx) }%, rgba(0,0,0,0) 100%)`;
+
+    document.getElementById("gradient-overlay-2").style.background =
+      `linear-gradient(90deg, rgba(0,0,0,0) 0%, rgb(255, 255, 255, 0.25) ${ this.calculateLighting(this.skewx) + 5 }%, rgba(0,0,0,0) 100%)`;
   }
 
   changeImage() {
@@ -60,18 +61,13 @@ export class ViewboxComponent implements OnInit {
       document.getElementById("shadow").setAttribute("src", `${this.imgsrc}`);
       document.getElementById("image-top").setAttribute("src", `${this.imgsrc}`);
     } else {
-      alert("You need a URL");
+      alert("You need an image URL!");
     }
   }
 
   calculateLighting(angle : number) : number {
-    //return (0.5 + (angle/80)/2)*100; //this is correct!
-    console.log((angle/80));
-    if(angle >= 0) {
-      return (0.5 + (angle/80)/2)*100; //this is correct!
-    } else {
-      return (0.5 + (angle/80)/2)*100; //This more less...
-    }
+    //The math function calculates the lighting overlay (0.5 + (angle/80)/2)*100 << TODO: Still needs calibration
+    return (0.5 + (angle/80)/2)*100;
   }
 
 }
